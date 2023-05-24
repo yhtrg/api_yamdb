658083d74f96867a74bea7f58809b820928fca1b
Артём Карташян <Ever-Kartashyan1234@yandex.ru>
2023-05-24 23:13:03 +0300

    hw10

--- api_yamdb/api/v1/serializers.py
+++ api_yamdb/api/v1/serializers.py
@@ -4,7 +4,6 @@ from rest_framework.exceptions import ValidationError
 
 from reviews.models import Category, Comment, Genre, Review, Title
 from users.models import User
-
 from api_yamdb.settings import EMAIL_LEN, USER_LEN
 
 
--- api_yamdb/api/v1/views.py
+++ api_yamdb/api/v1/views.py
@@ -9,11 +9,10 @@ from rest_framework import filters, permissions, response, status, viewsets
 from rest_framework.decorators import action, api_view
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
+
 from reviews.models import Category, Genre, Review, Title
 from users.models import User
-
 from api_yamdb.settings import DOMAIN_NAME
-
 from .filters import TitleFilter
 from .mixins import ListCreateDestroyViewSet
 from .permissions import IsAdmin, IsAdminUserOrReadOnly, IsAuthorOrAdmin
